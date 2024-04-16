7e412dfafeff54fdb480b14d2a6367b4267e5ca7
Andriy337 <91177781+Andriy337@users.noreply.github.com>
2024-04-17 00:50:55 +0300

    Update main.js

--- lab2/main.js
+++ lab2/main.js
@@ -1,18 +1,30 @@
 (function(window) {
-  var names = ["Jack", "Jill", "John", "Dasha", "Tanya", "Andriy", "Bohdan"];
+    
+    var names = ["Jack", "Jill", "John", "Dasha", "Tanya", "Andriy", "Bohdan"];
 
-  for (var i = 0; i < names.length; i++) {
-      var name = names[i];
-      var firstLetter = name.charAt(0).toLowerCase();
+    function greetsecond(name) {
+        var lastLetter = name.charAt(name.length - 1).toLowerCase();
+        
+        if (lastLetter === 'a') {
+            helloLibrary.greet(name);
+        } else {
+            byeLibrary.greet(name);
+        }
+    }
 
-      if (firstLetter === 'j') {
-          if (window.byeLibrary) {
-              byeLibrary.greet(name);
-          }
-      } else {
-          if (window.helloLibrary) {
-              helloLibrary.greet(name);
-          }
-      }
-  }
+
+    for (var i = 0; i < names.length; i++) {
+        var name = names[i];
+        var firstLetter = name.charAt(0).toLowerCase();
+
+        
+        if (firstLetter === 'j') {
+            byeLibrary.greet(name);
+        } else {
+            helloLibrary.greet(name);
+        }
+
+        
+        greetsecond(name);
+    }
 })(window);
